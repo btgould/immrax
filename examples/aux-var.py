d31899bf60e457a5def24971b355f393d7ab34e6
Brendan Gould <brendan.gould314@gmail.com>
2024-09-03 22:20:07 -0400

    SQUASH: broken AuxVarEmbeddedSys class

--- examples/aux-var.py
+++ examples/aux-var.py
@@ -1,25 +1,11 @@
 import immrax as irx
-from immrax.utils import I_refine, draw_iarray, null_space
-import jax
+from immrax.embedding import AuxVarEmbedding
+from immrax.utils import draw_iarray
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
-from immutabledict import immutabledict
 
 x0 = irx.icentpert(jnp.array([1, 0, 1]), jnp.array([0.1, 0.1, 0.2]))
-
-# Add auxiliary variable for interval refinement
 H = jnp.array([[1.0, 0.0], [0.0, 1.0], [1.0, 1.0]])
-N = null_space(H.T)
-A = N.T
-Hp = jnp.linalg.pinv(H) + A
-
-# Test if columns of A are in null space of H
-# print(jnp.all(jnp.isclose(A @ H, 0.0, atol=1e-5)))
-
-IH = jax.jit(I_refine(A))
-# print(I_refine(A)(irx.icentpert(jnp.array([1, 0, 1]), 0.1)))
-# print(H.shape)
-print(Hp @ jnp.array([1.0, 0.0, 1.0]))
 
 
 class HarmOsc(irx.System):
@@ -33,18 +19,13 @@ class HarmOsc(irx.System):
 
 
 # Create UT embedded system
-# FIXME: mjacif doesn't work here, I think it should
-olsys = HarmOsc()
-liftsys = irx.LiftedSystem(olsys, H, Hp)
-Fnat = jax.jit(irx.jacif(liftsys.f))
-embsys = irx.ifemb(liftsys, Fnat)
+embsys = AuxVarEmbedding(HarmOsc(), H)
 
 # Compute trajectory in embedding
 traj = embsys.compute_trajectory(
     0.0,
     1.56,
     irx.i2ut(x0),
-    f_kwargs=immutabledict({"refine": IH}),
 )
 
 # Clean up and display results
